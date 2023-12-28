2314d0fbb10cf88659e2ce251ec4ede0b935b78e
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-12-28 17:14:46 -0500

    Add requirements.txt file

--- cmscommerce/urls.py
+++ cmscommerce/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name="register"),
